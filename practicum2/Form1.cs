4c1131d820fe4666248a9811773db5c83d85b9dd
haydar100 <haydar.yilmaz@student.hu.nl>
2014-05-13 14:26:53 +0200

    final version

---
+++ practicum2/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace practicum2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+ 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = Int32.Parse(num1Text.Text);
+            int num2 = Int32.Parse(num2Text.Text);
+            int num3 = Int32.Parse(num3Text.Text);
+            
+            String output = MethodRunner.RunAllMethods(num1,num2,num3);
+            methodOutput.Text = output;
+
+            output = LambdaRunner.RunAllMethods(num1,num2,num3);
+            lambdaOutput.Text = output;
+
+            String[] lambdaOutputStrings = lambdaOutput.Text.ToString().Split('\n');
+            String[] methodOutputStrings = methodOutput.Text.ToString().Split('\n');
+
+
+
+            for (int i = 0; i < methodOutputStrings.Length - 1; i++)
+            {
+                string s = methodOutputStrings[i];
+                s = s.Substring(s.LastIndexOf("=") + 2);
+                methodOutputStrings[i] = s;
+
+                // lambdaOutputStrings[i] = lambdaOutputStrings[i].Substring(lambdaOutputStrings[i].IndexOf("=") + 2, lambdaOutputStrings.Length -1);
+
+            }
+
+            //
+           // Console.WriteLine(lambdaOutputStrings.Length.ToString());
+            for (int i = 0; i < lambdaOutputStrings.Length -1 ; i++)
+            {
+                string s = lambdaOutputStrings[i];
+               s = s.Substring(s.LastIndexOf("=") + 2);
+                lambdaOutputStrings[i] = s;
+                
+                // lambdaOutputStrings[i] = lambdaOutputStrings[i].Substring(lambdaOutputStrings[i].IndexOf("=") + 2, lambdaOutputStrings.Length -1);
+
+            }
+
+         
+
+            bool correct = true;
+
+            for (int i = 0; i < lambdaOutputStrings.Length - 1; i++)
+            {
+                if (!lambdaOutputStrings[i].Equals(methodOutputStrings[i]))
+                {
+                    correct = false;
+                    break;
+                }
+            }
+           
+         
+          /*   string[] lambdaOutputTextToStringArray = Regex.Split(lambdaOutput.Text, (@"=")); // krijgt 9 elementen?
+             string[] methodOutputTextToStringArray = methodOutput.Text.ToString().Split('='); // krijgt er ook 9 
+             int[] convertMethodOutput = Array.ConvertAll<string, int>(lambdaOutputTextToStringArray, int.Parse);
+             int[] convertLambdaOutput = Array.ConvertAll<string, int>(methodOutputTextToStringArray, int.Parse);
+             if (convertMethodOutput.Equals(convertLambdaOutput))
+             {
+                MessageBox.Show("Results are OK !");
+             }*/
+
+            if (correct)
+            {
+                MessageBox.Show("outputs are equal");
+            }
+            else
+            {
+                MessageBox.Show("not equal");
+            }
+        
+
+            // lijst maken en dan splitten en die 2 vergelijken ofzo. 
+
+
+
+
+        }
+    }
+}
---
+++ practicum2/LambdaRunner.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace practicum2
+{
+    class LambdaRunner
+    {
+        public static String RunAllMethods(int num1, int num2, int num3)
+        {
+            StringBuilder output = new StringBuilder();
+            Person p = new Person();
+            // methode TimesThree herschreven als lambda-expressie
+            Func<int, int> TimesThree = x => 3 * x;
+            output.AppendFormat("TimesThree({0}) = {1}\n", num1, TimesThree(num1));
+            Func<int, int, int, int> AddNumbers = (x, y, z) => x + y + z;
+            output.AppendFormat("AddNumbers({0},{1},{2}) = {3}\n", num1, num2, num3, AddNumbers(num1, num2, num3));
+            Func<int, bool> isEven = x => x %2 == 0;
+            output.AppendFormat("IsEven({0}) = {1}\n", num1, Methods.IsEven(num1));
+            Func<int, string> Num2String = x =>
+            {
+                switch (x)
+                {
+                    case 0:
+                        return "zero";
+                        
+                    case 1:
+                        return "one";
+                        
+                    case 2:
+                        return "two";
+                        
+                    case 3:
+                        return "three";
+                        
+                    case 4:
+                        return "four";
+                        
+                    case 5:
+                        return "five";
+                        
+                    case 6:
+                        return "six";
+                        
+                    case 7:
+                        return "seven";
+                        
+                    case 8:
+                        return "eight";
+                        
+                    case 9:
+                        return "nine";
+                        
+                    default:
+                        return "undefined";
+                      
+                }
+            };
+            output.AppendFormat("Num2String({0}) = {1}\n", num1, Num2String(num1));
+
+
+            Func<int, int, int, bool> InBetween = (x, y, z) => x < y && y < z;
+            output.AppendFormat("InBetween({0},{1},{2}) = {3}\n", num1, num2, num3, InBetween(num1, num2, num3));
+          
+            
+            Action<Person> ResetName = x => p.Name = null;
+            ResetName(p);
+            output.AppendFormat("ResetName, daarna (Name == null) = {0}\n", p.Name == null);
+
+
+     
+
+
+
+
+           
+            
+
+            //aanvullen
+
+            return output.ToString();
+        }
+
+    }
+}
